afd51eaeb3f53152b4dce238e6112ec0608ba467
Usman Khan <u.khan980212@gmail.com>
2026-01-18 00:08:20 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Navbar from "#components/Navbar";
+
+const App = () => {
+  return ( <main> <Navbar/> </main> );
+  }; 
+
+export default App;                                   
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import dayjs from "dayjs";
+
+// This works because importing a folder automatically resolves to index.js or index.ts.
+// You only need to be more specific when importing a non-default file.
+import { navLinks, navIcons } from "#constants";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="logo" />
+        <p className="font-bold">Usman's Portfolio</p>
+
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} className="icon-hover" alt={`icon-${id}`} />
+            </li>
+          ))}
+        </ul>
+
+        <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
